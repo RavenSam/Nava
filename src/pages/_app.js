d90aa299e464aea961a67915e14ddf0d6fe90a3b
RavenSam <maksam7dev@gmail.com>
2021-02-25 16:54:54 +0100

    added nprogress

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,15 @@ import { ChakraProvider } from "@chakra-ui/react"
 import Head from "next/head"
 import DefaultLayout from "../layouts/DefaultLayout"
 import { theme } from "../theme"
+import NextNprogress from "nextjs-progressbar"
+import NProgress from "nprogress"
 import "../styles/globals.css"
 
 export default function MyApp({ Component, pageProps }) {
    const Layout = Component.Layout ? Component.Layout : DefaultLayout
    const title = Component.title ? Component.title : "Nava WebSite"
+
+   NProgress.configure({ showSpinner: false })
 
    return (
       <>
@@ -19,6 +23,8 @@ export default function MyApp({ Component, pageProps }) {
          </Head>
 
          <ChakraProvider theme={theme}>
+            <NextNprogress color={theme.colors.primary[300]} height="3" startPosition={0.3} stopDelayMs={200} />
+
             <Layout>
                <Component {...pageProps} />
             </Layout>
